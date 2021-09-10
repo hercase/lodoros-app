dc0112ebb486d4d23a1fa2dd746b39f7c57d8443
Hernan Casé <hernan.case@globant.com>
2021-09-10 10:29:50 -0300

    add Lodoros logo

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Box w="100vw" bg="#000b15">
         <Container maxW="container.xl">
-          <Grid templateRows="3.5rem 1fr 3rem" gap={6} h="100vh" bg="brand.900">
+          <Grid templateRows="4rem 1fr 3rem" gap={6} h="100vh" bg="brand.900">
             <Header />
             <Box>
               <MainRoutes />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navigation from "./Navigation";
 import { Flex, Heading } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
+import { Image } from "@chakra-ui/image";
+
+import Logo from "../../assets/lodoros-logo.png";
 
 const Header = () => {
   return (
@@ -13,9 +16,7 @@ const Header = () => {
       px="4"
     >
       <Link to="/">
-        <Heading as="p" size="lg" color="white">
-          Lodoros App
-        </Heading>
+        <Image htmlWidth="237.5px" src={Logo} alt="logo" />
       </Link>
       <Navigation />
     </Flex>
